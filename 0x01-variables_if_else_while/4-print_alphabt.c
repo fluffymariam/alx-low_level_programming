cd902499f6cac39e0d45e18578d906e0afffd2de
fluffymariam <adefowobiladejobi@gmail.com>
2023-06-17 04:09:21 -0700

    task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
