f752607047e92371de8cf8511e7ace615c61f516
fluffymariam <adefowobiladejobi@gmail.com>
2023-06-17 06:52:43 -0700

    task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num1 = 0;
+	int num2;
+
+	while (num1 <= 8)
+	{
+		num2 = num1 + 1;
+	while (num2 <= 9)
+	{
+		putchar(num1 + '0');
+		putchar(num2 + '0');
+
+
+		if (num1 != 8 || num2 != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		num2++;
+	}
+
+	num1++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
