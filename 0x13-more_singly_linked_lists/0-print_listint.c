8efe97d66216d575305ec46a4b8066c868c298cd
fluffymariam <adefowobiladejobi@gmail.com>
2023-08-01 03:22:50 -0700

    m

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num =0;
+	size_t num = 0;
 
-	for (; h; h =h->next)
+	for (; h; h = h->next)
 	{
 		printf("%d\n", h->n);
-		num ++;
+		num++;
 	}
 
 	return (num);
