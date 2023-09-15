3be1a606f539b74c0e32b12feffbdc7105e85b56
fluffymariam <adefowobiladejobi@gmail.com>
2023-09-14 17:40:56 -0700

    cra

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position in the list.
- * @h: Pointer to the pointer to the head of the list.
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Pointer to a pointer to the head of the list.
  * @idx: Index where the new node should be added (starting from 0).
- * @n: Value to be assigned to the new node.
+ * @n: Value of the new node.
  *
  * Return: The address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int i = 0;
-    dlistint_t *new_node, *current = *h;
+	dlistint_t *new_node, *current;
+	unsigned int i;
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (h == NULL)
+		return (NULL);
 
-    while (current != NULL)
-    {
-        if (i == idx - 1)
-        {
-            new_node = malloc(sizeof(dlistint_t));
-            if (new_node == NULL)
-                return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-            new_node->n = n;
-            new_node->prev = current;
-            new_node->next = current->next;
-            
-            if (current->next != NULL)
-                current->next->prev = new_node;
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
-            current->next = new_node;
-            return (new_node);
-        }
-        
-        current = current->next;
-        i++;
-    }
+	current = *h;
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+		current = current->next;
 
-    return (NULL);
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->prev = current;
+	new_node->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
+
+	return (new_node);
 }
--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -1,33 +1,32 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    dlistint_t *head = NULL;
+	dlistint_t *head = NULL;
 
-    add_dnodeint_end(&head, 0);
-    add_dnodeint_end(&head, 1);
-    add_dnodeint_end(&head, 2);
-    add_dnodeint_end(&head, 3);
-    add_dnodeint_end(&head, 4);
-    add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
+	add_dnodeint_end(&head, 0);
+	add_dnodeint_end(&head, 1);
+	add_dnodeint_end(&head, 2);
+	add_dnodeint_end(&head, 3);
+	add_dnodeint_end(&head, 4);
+	add_dnodeint_end(&head, 98);
+	add_dnodeint_end(&head, 402);
+	add_dnodeint_end(&head, 1024);
 
-    print_dlistint(head);
-    printf("-----------------\n");
+	print_dlistint(head);
+	printf("-----------------\n");
 
-    insert_dnodeint_at_index(&head, 5, 4096);
+	insert_dnodeint_at_index(&head, 5, 4096);
+	print_dlistint(head);
 
-    print_dlistint(head);
-    free_dlistint(head);
-    head = NULL;
-
-    return (EXIT_SUCCESS);
+	free_dlistint(head);
+	head = NULL;
+	return (EXIT_SUCCESS);
 }
